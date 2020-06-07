d6be9618680cfebcb2ddbfb38c49e9bf5c04d6a0
김세진 <sejini17@gmail.com>
2020-06-07 20:24:10 +0900

    devServer proxy 설정추가

--- btv-demo.src/vue.config.js
+++ btv-demo.src/vue.config.js
@@ -8,5 +8,17 @@ module.exports = {
   publicPath: '/btv-demo/',
 
 
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: '<url>',
+        ws: true,
+        changeOrigin: true
+      },
+      '^/foo': {
+        target: '<other_url>'
+      }
+    }
+  }
 
 }
