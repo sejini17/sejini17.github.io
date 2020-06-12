589b50687fb19eb2c837990d97521f5426550ea2
김세진 <sejini17@bluedigm.com>
2020-06-12 14:02:22 +0900

    대화상자로 내용표시 추가

--- btv-demo.src/src/components/MovieList.vue
+++ btv-demo.src/src/components/MovieList.vue
@@ -11,13 +11,13 @@
                 height="300"
                 style="box-shadow: 0px 0px"
                 >
-                <v-carousel-item v-for="i in Math.floor(items.length/pageSize)+1" :key="i" >
+                <v-carousel-item v-for="i in pageCount(items.length)" :key="i" >
 
                   <v-layout row >
                     <v-flex pl-2 pr-2 
                       class="d-flex flex-row">
 
-                      <v-card v-for="item in items.slice((i-1)*pageSize, (i-1)*pageSize+pageSize)" :key="item.src" 
+                      <v-card v-for="item in curPageItems(items, i)" :key="item.src" 
                         class="mx-auto "
                         max-width="100"
                         ripple
@@ -31,7 +31,7 @@
                       </v-card>
 
                       <!-- 공백 채우기용 -->
-                      <v-card v-for="i in (pageSize - items.slice((i-1)*pageSize, (i-1)*pageSize+pageSize).length )" :key="i" 
+                      <v-card v-for="i in remainItemCount(items, i)" :key="i" 
                         class="mx-auto"
                         width="100"
                         max-height="100"
@@ -59,6 +59,51 @@
           </v-card>
         </v-row>
 
+
+    <v-dialog
+      v-model="showDialog"
+      width="500"
+    >
+<!--     
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template> -->
+
+      <v-card>
+        <v-card-title
+          class="headline grey"
+          primary-title
+        >
+          {{dTitle}}
+        </v-card-title>
+
+        <v-card-text>{{dContent}}
+          <br/>
+          《정직한 후보》는 장유정 감독의 2020년 대한민국의 코미디 영화이자, 세 번째 장편 연출작이다. 2014년 브라질에서 개봉한 동명의 영화이자 박스오피스 1위를 기록하였던 《O Candidato Honesto》의 리메이크작이다.<br/>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="showDialog = false"
+          >
+            닫기
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     </v-container>
 </template>
 
@@ -73,7 +118,13 @@
     ],
     data: () => ({
       selectedItem: null,
+
+      showDialog: false,
+      dTitle: "",
+      dContent: ""
     }),
+    computed: {
+    },
 
     created () {
     },
@@ -92,6 +143,10 @@
         }
         this.highlightItem(item)
 
+        this.dTitle = item.id
+        this.dContent = item.src
+        this.showDialog = !this.showDialog
+
         this.$emit('selected', item.id)
       },
 
@@ -102,6 +157,16 @@
 // console.log("highlightItem : " + item.id)
         }
         selected.blur = false
+      },
+      
+      pageCount(totCount) {
+        return Math.floor(totCount /this.pageSize) +1
+      },
+      curPageItems(items, i) {
+        return items.slice((i-1) *this.pageSize, (i-1) *this.pageSize +this.pageSize)
+      },
+      remainItemCount(items, i) {
+        return this.pageSize - this.curPageItems(items, i).length
       }
 
     },
